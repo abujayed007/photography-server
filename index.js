e84f7fb0c3422f6432ccd6a0e1f6c67189d804ab
abujayed007 <abujayed007@gmail.com>
2022-11-10 14:52:06 +0600

    review server completed

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5001 ;
 require('dotenv').config();
 
-app.use(express());
+app.use(express.json());
 app.use(cors());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lugl172.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run (){
     try{
+  const serviceCollection = client.db("photography").collection("service");
+ const cliendReviews = client.db("photography").collection("clientReviews");
 
+  // service items
+  app.get('/services',  async (req, res)=>{
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
+    const query = {}
+    const cursor = serviceCollection.find(query)
+    const services = await cursor.skip(page * size).limit(size).toArray()
+    const count = await serviceCollection.estimatedDocumentCount();
+    res.send({services, count})
+  });
+
+  app.get('/services/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = { _id: ObjectId(id)};
+    const service = await serviceCollection.findOne(query);
+    res.send(service)
+  });
+
+  //  review items
+  app.get('/review', async (req, res) => {
+    let query = {};
+    if (req.query.email) {
+        query = {
+            email: req.query.email
+        }
+    }
+
+    const cursor = cliendReviews.find(query);
+    const orders = await cursor.toArray();
+    res.send(orders);
+});
+  app.post('/review', async (req, res)=>{
+    const review = req.body;
+    console.log(review);
+    const result = await cliendReviews.insertOne(review);
+    res.send(result)
+  });
     }
     finally{
 
