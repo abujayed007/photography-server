4779433389eba174b9a3d8b5d31437a067e2f67f
abujayed007 <abujayed007@gmail.com>
2022-11-11 22:11:27 +0600

    updated

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function run (){
   });
   
   app.delete('/review/:id', async(req, res)=>{
-    const id = req.params.id
+    const id = req.params.id;
     const query = {_id: ObjectId(id)}
     const result = await cliendReviews.deleteOne(query)
     res.send(result)
