12aee130e12d7c28821223af7d5282d1ccb0fd12
abujayed007 <abujayed007@gmail.com>
2022-11-11 22:34:34 +0600

    resolved

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lugl172.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
